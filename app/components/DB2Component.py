3473b7de22ac889608faa6dd3c2de27d7d400164
gregory <gcvalderrama@hotmail.com>
2020-08-13 14:31:50 -0500

    add

---
+++ app/components/DB2Component.py
@@ -0,0 +1,23 @@
+from app.entities.DB2ConnectionEntity import DB2ConnectionEntity
+
+
+class ConnectionComponent:
+
+    @staticmethod
+    def validate_connection( entity: DB2ConnectionEntity):
+        conn = None
+        try:
+            conn = db.connect(entity.get_connection(), "", "")
+            cursor = conn.cursor()
+            cursor.execute("SELECT service_level FROM  TABLE(sysproc.env_get_inst_info()) as INSTANCEINFO")
+            cursor.fetchall()
+            cursor.close()
+            return True
+        except Exception as e:
+            raise ValueError(entity.get_connection()) from e
+        finally:
+            if conn:
+                try:
+                    conn.close()
+                except:
+                    raise ValueError("Close Exception {}".format(entity.get_connection()))
